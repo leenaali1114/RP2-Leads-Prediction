a8198b5fc2978e48a26c801dfa4eb0f7f4afa41f
Leena Ali <67731093+leenaali1114@users.noreply.github.com>
2023-12-27 17:59:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -53,26 +53,30 @@ def main():
         #     # Create DataFrame
         #     course_tab = pd.DataFrame(tab)
         #     st.write(course_tab)
-        html_temp = """<div class='tableauPlaceholder' id='viz1703652666628' style='position: relative'><noscript><a href='#'>
-                                    <img alt='RP2 Sales Statistics ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;
-                                    images&#47;RP&#47;RP2BDEPerformance&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a>
-                                    </noscript><object class='tableauViz'  style='display:none;'>
+        html_temp = """<div class='tableauPlaceholder' 
+                            id='viz1703679862775' 
+                            style='position: relative'>
+                            <noscript><a href='#'>
+                            <img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;
+                            images&#47;Ta&#47;TableuRP2&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a>
+                            </noscript><object class='tableauViz'  style='display:none;'>
                         <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
                         <param name='embed_code_version' value='3' /> 
                         <param name='site_root' value='' />
-                        <param name='name' value='RP2BDEPerformance&#47;Dashboard1' />
+                        <param name='name' value='TableuRP2&#47;Dashboard1' />
                         <param name='tabs' value='no' />
                         <param name='toolbar' value='yes' />
-                        <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;RP&#47;RP2BDEPerformance&#47;Dashboard1&#47;1.png' /> 
+                        <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ta&#47;TableuRP2&#47;Dashboard1&#47;1.png' /> 
                         <param name='animate_transition' value='yes' />
                         <param name='display_static_image' value='yes' />
                         <param name='display_spinner' value='yes' />
                         <param name='display_overlay' value='yes' />
                         <param name='display_count' value='yes' />
-                        <param name='language' value='en-US' /></object></div>    
+                        <param name='language' value='en-US' />
+                        <param name='filter' value='publish=yes' /></object></div> 
 
                         <script type='text/javascript'>                    
-                                    var divElement = document.getElementById('viz1703652666628');                    
+                                    var divElement = document.getElementById('viz1703679862775');                    
                                     var vizElement = divElement.getElementsByTagName('object')[0];                    
                                     if ( divElement.offsetWidth > 800 ) { 
                                         vizElement.style.width='100%';
@@ -82,9 +86,9 @@ def main():
                                         vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} 
                                     else { 
                                         vizElement.style.width='100%';
-                                        vizElement.style.height='1527px';}                     
+                                        vizElement.style.height='1877px';}                     
                                     var scriptElement = document.createElement('script');                    
-                                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
+                                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
                                     vizElement.parentNode.insertBefore(scriptElement, vizElement);                
                         </script>"""
         
